8f717f291b915564523d8e66f57d221f7ec53666
Hedgy134117 <hedgy134117@gmail.com>
2020-04-08 12:52:12 -0400

    remove onlineDice

--- diceSlaveOnline/diceSlaveOnline/settings.py
+++ diceSlaveOnline/diceSlaveOnline/settings.py
@@ -140,13 +140,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# LOCAL DEVELOPMENT
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# ONLINE DEVELOPMENT
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
